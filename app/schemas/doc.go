e0043a49a3767463876124f2d6bdc868bc965eb9
yagrush <yagrush@users.noreply.github.com>
2024-04-10 22:30:07 +0900

    oapi-codegenを適用してみた

---
+++ app/schemas/doc.go
@@ -0,0 +1,3 @@
+package schemas
+
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen --config=config.yaml schemas.yaml
--- app/usecase/sample_calc_add_int64.go
+++ app/usecase/sample_calc_add_int64.go
@@ -7,38 +7,42 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/yagrush/go-sample-restapi/app/domain/repository"
+	"github.com/yagrush/go-sample-restapi/app/schemas"
 )
 
 type SampleCalcAddInt64Usecase struct {
 	R repository.SampleRepository
 }
 
-func (u *SampleCalcAddInt64Usecase) Serve(c *gin.Context) (int, gin.H) {
+func (u *SampleCalcAddInt64Usecase) Serve(c *gin.Context, params schemas.SampleCalcAddInt64Params) {
 	aStr := c.Request.FormValue("a")
 	a, err := strconv.ParseInt(aStr, 10, 64)
 	if err != nil {
-		return http.StatusInternalServerError, gin.H{
+		c.JSON(http.StatusInternalServerError, gin.H{
 			"errorMessage": fmt.Errorf("invalid request parameter a=%s", aStr),
-		}
+		})
+		return
 	}
 
 	bStr := c.Request.FormValue("b")
 	b, err := strconv.ParseInt(bStr, 10, 64)
 	if err != nil {
-		return http.StatusInternalServerError, gin.H{
+		c.JSON(http.StatusInternalServerError, gin.H{
 			"errorMessage": fmt.Errorf("invalid request parameter b=%s", bStr),
-		}
+		})
+		return
 	}
 
 	ret, err := u.R.CalcAddInt64(c, a, b)
 	if err != nil {
-		return http.StatusInternalServerError, gin.H{
+		c.JSON(http.StatusInternalServerError, gin.H{
 			"errorMessage": fmt.Errorf("error: CalcAddInt64"),
-		}
+		})
+		return
 	}
 
-	// fmt.Fprint(c.Writer, ret)
-	return http.StatusOK, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"message": ret,
-	}
+	})
+	return
 }
